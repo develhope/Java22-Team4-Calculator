0aa01cd6047a0a209300641292ff847b3a9f99cd
Jamming1 <154436983+Jamming1@users.noreply.github.com>
2024-01-23 12:59:26 +0100

    Add files via upload

---
+++ Java22-Team4-Calculator/src/Division.java
@@ -0,0 +1,15 @@
+public class Division {
+
+    public static void main (String[] args){
+
+    System.out.println(divisionMethod(12,66));
+
+    }
+
+   //metodo che definisce la divisione:
+    public static float divisionMethod(float firstValue, float secondValue){
+
+        return firstValue / secondValue;
+
+    }
+}
