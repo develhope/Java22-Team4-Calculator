b23ffd2cbd17bb8d27fb8fb1f45c785e18d09f7f
martacaponnetto <154430256+martacaponnetto@users.noreply.github.com>
2024-01-23 17:29:29 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
-// Metodo per il calcolo della sottrazione di due valori
-    public static int somma(int valore1, int valore2) {
-        // Calcola la sottrazione e restituisce il risultato
-        int risultato = valore1 + valore2;
-        return risultato;
-    }
+public class Main {
 
+    public static void main(String[] args) {
+        int risultatoSomma = somma(6, 3);
+        System.out.println("Somma: " + risultatoSomma);
+    }
+    
+    public static int somma(int valore1, int valore2) {
+    int risultato = valore1 + valore2;
+    return risultato;
+    }
+} 
