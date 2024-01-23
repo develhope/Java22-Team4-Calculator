84e2dc357d5bb7da94229d4968afe07eff75c2cf
martacaponnetto <martacaponnetto@yahoo.it>
2024-01-23 15:38:12 +0100

    add sub

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,11 @@ public class Main {
     public static void main(String[] args) {
 
     }
-
+    public static int sottrazione(int valore1, int valore2) {
+        // Calcola la sottrazione e restituisce il risultato
+        int risultato = valore1 - valore2;
+        return risultato;
+    }
 }
+
+
