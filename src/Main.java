28b2f1491fa01bc6b74c203ca44e0415eb26747c
martacaponnetto <154430256+martacaponnetto@users.noreply.github.com>
2024-01-23 17:31:18 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int risultatoSottrazione = sottrazione(6, 3);
+        System.out.println("Sottrazione: " + risultatoSottrazione);
+    }
 
     public static int sottrazione(int valore1, int valore2) {
-        // Calcola la sottrazione e restituisce il risultato
         int risultato = valore1 - valore2;
         return risultato;
     }
-
+}
 
 
