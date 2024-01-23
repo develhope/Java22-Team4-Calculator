e286ab0fd56b081ccf3b5588251d95def4d2f84c
martacaponnetto <martacaponnetto@yahoo.it>
2024-01-23 16:26:13 +0100

    add addition

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
-
     public static void main(String[] args) {
 
+        int risultatoSomma = somma(6, 3);
+        System.out.println("Somma: " + risultatoSomma);
     }
 
+    // Metodo per il calcolo della sottrazione di due valori
+    public static int somma(int valore1, int valore2) {
+        // Calcola la sottrazione e restituisce il risultato
+        int risultato = valore1 + valore2;
+        return risultato;
+    }
 }
