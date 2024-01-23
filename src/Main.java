672160898a670417654ec1fd71bd57594ae16a7e
danieleocchi <dennyais146@gmail.com>
2024-01-23 22:25:29 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
-
     public static void main(String[] args) {
-
+        int x = 5;
+        int y = 10;
+        System.out.println("i numeri moltiplicati sono pari a " +multipNumbers(x, y));
     }
 
+    //il metodo seguente restituisce il valore della moltiplicazione di due numeri
+    static int multipNumbers(int x, int y) {
+        return (x * y);
+    }
 }
