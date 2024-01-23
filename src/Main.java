8e9eefe723947ba206e11e7d95b69063d29b0c76
danieleocchi <dennyais146@gmail.com>
2024-01-23 22:25:29 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        int x = 5;
+        System.out.println("il numero è " +x);
+        System.out.println("il numero è pari?");
+        checkEven(x);
     }
+//Un metodo che preveda un valore in ingresso e
+// determini se quel valore è pari o dispari
+// e restituendo il tipo.
 
+    static void checkEven(int x) {
+        if(x%2 == 0) {
+            System.out.println(x + " e' pari.");
+        }
+        else {
+            System.out.println(x + " e' dispari.");
+        }
+    }
 }
