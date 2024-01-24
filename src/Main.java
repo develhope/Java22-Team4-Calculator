c2b4645b4ae7cce91234e5be0fe3886c125fd6ae
martacaponnetto <154430256+martacaponnetto@users.noreply.github.com>
2024-01-24 17:54:46 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ public class Main {
     }
 
     public static int sottrazione(int valore1, int valore2) {
-        int risultato = valore1 - valore2;
-        return risultato;
+          return valore1 - valore2;
+        
     }
 }
 
