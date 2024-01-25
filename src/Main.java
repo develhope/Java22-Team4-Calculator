f3a0fb7d9e3aad4136714b80fa3017534d6d6241
Jamming1 <154436983+Jamming1@users.noreply.github.com>
2024-01-25 09:51:00 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,36 @@
 public class Main {
+public static void main (String[] args){
 
-    public static void main(String[] args) {
+        System.out.println(divisionMethod(12,66));
+        System.out.println(moltiplicationMethod(13,6));
+        System.out.println(additionMethod(2,9));
+        System.out.println(subtrationMethod(34,8));
 
     }
 
+    //metodo che definisce la divisione:
+    public static float divisionMethod(float firstValue, float secondValue){
+
+        return firstValue / secondValue;
+
+    }
+    //metodo che definisce la moltiplicazione:
+    public static float moltiplicationMethod(float firstValue, float secondValue){
+
+        return firstValue * secondValue;
+
+    }
+    //metodo che definisce l'addizione:
+    public static float additionMethod(float firstValue, float secondValue){
+
+        return firstValue + secondValue;
+
+    }
+
+    //metodo che definisce la sottrazione:
+    public static float subtrationMethod(float firstValue, float secondValue){
+
+        return firstValue - secondValue;
+
+    }
 }
