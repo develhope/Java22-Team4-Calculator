601e7e7ee7acf6a6173de3291971d41e1d491cd5
Jamming <j.lucreziaparrillo@gmail.com>
2024-02-01 18:32:32 +0100

    Main method aggiornato

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,7 @@ public static void main (String[] args){
         System.out.println(moltiplicationMethod(13,6));
         System.out.println(additionMethod(2,9));
         System.out.println(subtrationMethod(34,8));
-   // addition
-    int risultatoSomma = somma(6, 3);
-    System.out.println("Somma: " + risultatoSomma);
 }
-
-    public static int somma(int valore1, int valore2) {
-        return valore1 + valore2;
-    }
-
 
     //metodo che definisce la divisione:
     public static float divisionMethod(float firstValue, float secondValue){
