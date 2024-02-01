e190ca78b0a07abfbdf615193841cfdd1dae7e69
martacaponnetto <martacaponnetto@yahoo.it>
2024-02-01 18:47:07 +0100

    add Addixione

---
+++ src/Addixione.java
@@ -0,0 +1,14 @@
+public class Addixione {
+    public static void main(String[] args) {
+
+        System.out.println("Addixione: " + addixione(5, 3));
+        }
+
+
+
+        public static int addixione(int valore1, int valore2) {
+         return  valore1 + valore2;
+        }
+    }
+
+
