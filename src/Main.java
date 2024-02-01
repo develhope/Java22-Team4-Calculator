e676478184eb2f56ccf9bac571ed24b9458bfb6b
vincenzo <vincenzo.costagliola@develhope.co>
2024-02-01 10:42:50 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
         public static void main(String[] args) {
-            int x = 5;
-            int y = 10;
+            int x = 6;
+            int y = 11;
             System.out.println("il numero è " +x);
             System.out.println("il numero è pari?");
             checkEven(x);
