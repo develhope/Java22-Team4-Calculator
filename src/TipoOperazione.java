666273566c4fc7e6ea8a87c6d077109b7f176ba5
halaaniba <156901595+halaaniba@users.noreply.github.com>
2024-02-01 11:49:50 +0100

    Initial commit

---
+++ src/TipoOperazione.java
@@ -0,0 +1,26 @@
+public class TipoOperazione{
+
+    public static void main(String[] args) {
+        String operazione = "1:2=";
+        String tipo = determinaTipoOperazione(operazione);
+        System.out.println("Tipo di operazione: " + tipo);
+    }
+
+    public static String determinaTipoOperazione(String operazione) {
+        if (operazione.contains("+")) {
+            return "SOMMA";
+        } else if (operazione.contains("-")) {
+            return "DIFFERENZA";
+        } else if (operazione.contains("*")) {
+            return "MOLTIPLICAZIONE";
+        } else if (operazione.contains("/")) {
+            return "DIVISIONE";
+        } else if (operazione.contains("^")) {
+            return "POTENZA";
+        } else {
+            // Tipo di operazione non riconosciuto
+            return "OPERAZIONE NON SUPPORTATA";
+        }
+    }
+}
+
