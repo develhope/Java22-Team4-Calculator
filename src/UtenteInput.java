148fc1650d05c10d8f2bcf0dc4bd53f925d70b0f
Jamming <j.lucreziaparrillo@gmail.com>
2024-02-02 11:25:47 +0100

    utente method aggiornato

--- src/UtenteInput.java
+++ src/UtenteInput.java
@@ -4,26 +4,31 @@ public class UtenteInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Inserire primo numero");
-        int x = scanner.nextInt();
-        System.out.println("Inserire secondo numero");
-        int y = scanner.nextInt();
+        double x = scanner.nextInt();
         System.out.println("Inserire tipo di operatore");
         String operatore = scanner.next();
+        System.out.println("Inserire secondo numero");
+        double y = scanner.nextInt();
 
-        if (Objects.equals(operatore, "/")) {
-            int division = x / y;
-            System.out.println("risultato della divisione= " + division);
 
+        if (Objects.equals(operatore, "/")){
+            try {
+                double division = x / y;
+                System.out.println("risultato della divisione= " + division);
+            }catch(ArithmeticException e){
+                System.out.println("non è possibile dividere per 0");
+            }
         } else if (Objects.equals(operatore, "*")) {
-            int moltiplication = x * y;
+            double moltiplication = x * y;
             System.out.println("risultato della moltiplicazione= " + moltiplication);
         } else if (Objects.equals(operatore, "+")) {
-            int add = x + y;
+            double add = x + y;
             System.out.println("risultato della addizione= " + add);
         } else if (Objects.equals(operatore, "-")) {
-            int sub = x - y;
+            double sub = x - y;
             System.out.println("risultato della sottrazione= " + sub);
         }
         scanner.close();
+        System.out.println("\nCiao <3");
     }
 }
